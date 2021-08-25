bfb26bd08af62cc8bc017a66e276affd55e44ff2
Clayton Pereira <clayton.pereira@zup.com.br>
2021-08-25 05:05:52 -0300

    Projeto - Kotlin-Micronaut Diferentes Ambientes Test-Dev-Prod e Isolamento de Testes

--- casadocodigo/src/main/kotlin/br/com/zupacademy/Application.kt
+++ casadocodigo/src/main/kotlin/br/com/zupacademy/Application.kt
@@ -1,10 +1,12 @@
 package br.com.zupacademy
 
-import io.micronaut.runtime.Micronaut.*
+import io.micronaut.runtime.Micronaut.build
+
 fun main(args: Array<String>) {
-	build()
-	    .args(*args)
-		.packages("br.com.zupacademy")
-		.start()
+    build()
+        .args(*args)
+        .packages("br.com.zupacademy")
+        .defaultEnvironments("dev")
+        .start()
 }
 
--- casadocodigo/src/test/kotlin/br/com/zupacademy/autor/autorbuscaemail/AutorBuscaEmailControllerTest.kt
+++ casadocodigo/src/test/kotlin/br/com/zupacademy/autor/autorbuscaemail/AutorBuscaEmailControllerTest.kt
@@ -9,6 +9,7 @@ import br.com.zupacademy.autor.autorcadastro.enderecoclient.EnderecoResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.annotation.TransactionMode
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -17,8 +18,22 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import javax.inject.Inject
 
-@MicronautTest //executa a classe no contexto micronaut
+@MicronautTest(  //executa a classe no contexto micronaut
+    //Controle Transacional para melhor Isolamento de Teste
+    rollback = false, //Commita
+    transactionMode = TransactionMode.SINGLE_TRANSACTION, //default: SEPARATE_TRANSACTION (@BeforeEach e @Test mesma transação)
+    transactional = false //AutoCommit
+)
+
 internal class AutorBuscaEmailControllerTest {
+
+    /**
+     * ESTRATEGIAS de ISOLAMENTO
+     * Louça: Sujou, Limpou -> @AfterEach
+     * Louça: Limpou, Usou -> @BeforeEach [*]
+     * Louça: Usa Louça Descatável -> rollback=true
+     * Louça: Usou a Louça, Joga Fora, Compra Outra -> recria banco de teste (h2)
+     */
 
     @field:Inject
     lateinit var autorRepositoy: AutorRepositoy
@@ -34,12 +49,12 @@ internal class AutorBuscaEmailControllerTest {
         val endereco: Endereco = Endereco(EnderecoResponse("Praça José Pedro de Alcântara", "Casa", "Parque Oratório", "Santo André", "SP"))
         autor = Autor("Clayton Pereira", "clayton.pereira@zup.com.br", "Engenheiro de Software", endereco)
         autorRepositoy.save(autor)
-    }
+    } //commit
 
-    @AfterEach
+    @AfterEach //limpa o banco a cada transacao
     internal fun tearDown() {
         autorRepositoy.deleteAll()
-    }
+    } //commit - transação independente
 
     @Test
     internal fun deveRetornarOsDetalhesDeUmAutor() {
@@ -54,5 +69,5 @@ internal class AutorBuscaEmailControllerTest {
         assertEquals(autor.descricao, response.body()!!.descricao)
         assertEquals(autor.email, response.body()!!.email)
 
-    }
+    } // commitada por conta do rollback= false
 }
